51e71ae7fa868ca4b68a0e88f1a27c2d6faeb7a0
Ezz1997 <ezmarie97@gmail.com>
2026-04-09 20:37:21 +0300

    Add toggle read status prototype function and make it so the user dynamically changes its value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = new Map();
 
 function Book(title, author, pagesNum, isRead) {
-  if(!new.target){
+  if (!new.target) {
     throw Error("Use new before creating an object");
   }
 
@@ -12,6 +12,10 @@ function Book(title, author, pagesNum, isRead) {
   this.isRead = isRead;
 }
 
+Book.prototype.toggleReadStatus = function(){
+  this.isRead = !this.isRead;
+};
+
 function addBookToLibrary(bookTitle, author, pagesNum, isRead) {
   // take params, create a book then store it in the array
   let book = new Book(bookTitle, author, pagesNum, isRead);
@@ -21,14 +25,16 @@ function addBookToLibrary(bookTitle, author, pagesNum, isRead) {
   createBookCard(book);
 }
 
-function createBookCard(book){
+function createBookCard(book) {
   const cardsContainer = document.querySelector("#cards-container");
   let card = document.createElement("div");
   const titleElement = document.createElement("h1");
   const authorElement = document.createElement("p");
   const pagesNumElement = document.createElement("p");
-  const isReadElement = document.createElement("p");
+  const isReadElement = document.createElement("input");
   const removeButtonElement = document.createElement("button");
+  const checkboxWrapperDiv = document.createElement("div");
+  const checkBoxLabel = document.createElement("label");
 
   card.style.border = "1px solid black";
   card.style.height = "300px";
@@ -37,21 +43,35 @@ function createBookCard(book){
   titleElement.textContent = `Title: ${book.title}`;
   authorElement.textContent = `Author: ${book.author}`;
   pagesNumElement.textContent = `Pages Num: ${book.pagesNum}`;
-  isReadElement.textContent = `IsRead: ${book.isRead ? "Yes" : "No"}`;
+
+  checkBoxLabel.for = "isReadElement";
+  checkBoxLabel.textContent = "Is Read";
+
+  isReadElement.type = "checkbox";
+  isReadElement.id = "isReadElement";
+  isReadElement.checked = book.isRead;
+
+  isReadElement.addEventListener("click", (e) => {
+    book.toggleReadStatus();
+  })
+
+  checkboxWrapperDiv.append(checkBoxLabel);
+  checkboxWrapperDiv.append(isReadElement);
+
   removeButtonElement.textContent = "Remove";
   removeButtonElement.classList.add("remove-btn");
 
-  removeButtonElement.addEventListener("click", () =>{
-    let bookId = card.getAttribute("data-index-number");
+  removeButtonElement.addEventListener("click", () => {
+    const bookId = card.getAttribute("data-index-number");
     myLibrary.delete(bookId);
     console.log(myLibrary);
     card.remove();
   });
-  
+
   card.appendChild(titleElement);
   card.appendChild(authorElement);
   card.appendChild(pagesNumElement);
-  card.appendChild(isReadElement);
+  card.appendChild(checkboxWrapperDiv);
   card.appendChild(removeButtonElement);
 
   card.setAttribute("data-index-number", book.id);
@@ -59,8 +79,8 @@ function createBookCard(book){
   cardsContainer.appendChild(card);
 }
 
-function displayAllBooks(){
-  for(let book of myLibrary.values()){
+function displayAllBooks() {
+  for (let book of myLibrary.values()) {
     createBookCard(book);
   }
 }
@@ -71,7 +91,7 @@ bookForm.addEventListener("submit", (e) => {
   const data = new FormData(e.target);
   const values = Object.fromEntries(data.entries());
 
-  addBookToLibrary(values["book-title"], values["book-author"], 
-                   values["pages-num"], values["isRead"]);
+  addBookToLibrary(values["book-title"], values["book-author"],
+    values["pages-num"], values["isRead"]);
 });
 
